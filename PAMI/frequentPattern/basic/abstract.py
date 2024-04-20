9463044c750d3bb4ad55859410a8ac29384525d8
vanitha <vanithakattumuri32@gmail.com>
2024-04-20 12:16:42 +0900

    #1 updated basic files

--- PAMI/frequentPattern/basic/abstract.py
+++ PAMI/frequentPattern/basic/abstract.py
@@ -1,10 +1,52 @@
-
+#  Copyright (C)  2021 Rage Uday Kiran
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 from abc import ABC as _ABC, abstractmethod as _abstractmethod
+import time as _time
+import csv as _csv
+import pandas as _pd
+from collections import defaultdict as _defaultdict
+from itertools import combinations as _c
+import os as _os
+import os.path as _ospath
+import psutil as _psutil
+import sys as _sys
+import validators as _validators
+from urllib.request import urlopen as _urlopen
+import functools as _functools
 
 
 class _frequentPatterns(_ABC):
+
+
+
+
 
 
     def __init__(self, iFile, minSup, sep="\t"):
@@ -19,9 +61,62 @@ class _frequentPatterns(_ABC):
         self._startTime = float()
         self._endTime = float()
 
+    '''@abstractmethod
+    def iFile(self):
+        """Variable to store the input file path/file name"""
+
+        pass
+
+    @abstractmethod
+    def minSup(self):
+        """Variable to store the user-specified minimum support value"""
+
+        pass
+
+    @abstractmethod
+    def sep(self):
+        """Variable to store the user-specified minimum support value"""
+
+        pass
+
+    @abstractmethod
+    def startTime(self):
+        """Variable to store the start time of the mining process"""
+
+        pass
+
+    @abstractmethod
+    def endTime(self):
+        """Variable to store the end time of the complete program"""
+
+        pass
+
+    @abstractmethod
+    def memoryUSS(self):
+        """Variable to store USS memory consumed by the program"""
+
+        pass
+
+    @abstractmethod
+    def memoryRSS(self):
+        """Variable to store RSS memory consumed by the program"""
+
+        pass
+
+    @abstractmethod
+    def finalPatterns(self):
+        """Variable to store the complete set of patterns in a dictionary"""
+
+        pass
+
+    @abstractmethod
+    def oFile(self):
+        """Variable to store the name of the output file to store the complete set of frequent patterns"""
+
+        pass'''
+
     @_abstractmethod
     def startMine(self):
-
         pass
 
     @_abstractmethod
@@ -42,23 +137,19 @@ class _frequentPatterns(_ABC):
     @_abstractmethod
     def getMemoryUSS(self):
 
-
         pass
 
     @_abstractmethod
     def getMemoryRSS(self):
-
 
         pass
 
     @_abstractmethod
     def getRuntime(self):
 
-
         pass
 
     @_abstractmethod
     def printResults(self):
-
 
         pass
