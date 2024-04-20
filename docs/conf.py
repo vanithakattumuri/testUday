8788b84a1118eaf6fc57c6b2aed924434fe383e0
vanitha <vanithakattumuri32@gmail.com>
2024-04-20 16:11:10 +0900

    #1 updated basic files

--- docs/conf.py
+++ docs/conf.py
@@ -19,17 +19,13 @@ release = 'a'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon', 'sphinx.ext.duration']
-
-
-
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon', 'sphinx.ext.duration', "sphinx.ext.intersphinx", 'sphinx.ext.doctest']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-# html_static_path = ['_static']
+html_static_path = ['_static']
