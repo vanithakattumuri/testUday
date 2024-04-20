11335e4428a0afcc411987aedb8f3b81d3a077fc
vanitha <vanithakattumuri32@gmail.com>
2024-04-20 16:57:57 +0900

    #1 updated basic files

--- PAMI1/frequentPattern/basic/Apriori.py
+++ PAMI1/frequentPattern/basic/Apriori.py
@@ -31,43 +31,138 @@
 #
 
 
+__copyright__ = """
+Copyright (C)  2021 Rage Uday Kiran
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from PAMI1.frequentPattern.basic import abstract as _ab
 from typing import Dict, Union
 from deprecated import deprecated
 
 
 class Apriori(_ab._frequentPatterns):
+    """
+    :Description: Apriori is one of the fundamental algorithm to discover frequent patterns in a transactional database. This program employs apriori property (or downward closure property) to  reduce the search space effectively. This algorithm employs breadth-first search technique to find the complete set of frequent patterns in a transactional database.
+
+    :Reference:  Agrawal, R., Imieli ́nski, T., Swami, A.: Mining association rules between sets of items in large databases.
+            In: SIGMOD. pp. 207–216 (1993), https://doi.org/10.1145/170035.170072
+
+    :param  iFile: str :
+                   Name of the Input file to mine complete set of frequent patterns
+    :param  oFile: str :
+                   Name of the output file to store complete set of frequent patterns
+    :param  minSup: int or float or str :
+                   The user can specify minSup either in count or proportion of database size. If the program detects the data type of minSup is integer, then it treats minSup is expressed in count. Otherwise, it will be treated as float.
+    :param  sep: str :
+                   This variable is used to distinguish items from one another in a transaction. The default seperator is tab space. However, the users can override their default separator.
+
+
+
+    :Attributes:
+
+        startTime : float
+          To record the start time of the mining process
+
+        endTime : float
+          To record the completion time of the mining process
+
+        finalPatterns : dict
+          Storing the complete set of patterns in a dictionary variable
+
+        memoryUSS : float
+          To store the total amount of USS memory consumed by the program
+
+        memoryRSS : float
+          To store the total amount of RSS memory consumed by the program
+
+        Database : list
+          To store the transactions of a database in list
+
+
+
+    **Methods to execute code on terminal**
+      -----------------------------------
+
+    .. code-block:: console
+
+      Format:
+
+      (.venv) $ python3 Apriori.py <inputFile> <outputFile> <minSup>
+
+      Example Usage:
+
+      (.venv) $ python3 Apriori.py sampleDB.txt patterns.txt 10.0
+
+    .. note:: minSup will be considered in percentage of database transactions
+
+
+    **Importing this algorithm into a python program**
+    ---------------------------------------------------
+
+    .. code-block:: python
+
+            import PAMI1.frequentPattern.basic.Apriori as alg
+
+            obj = alg.Apriori(iFile, minSup)
+
+            obj.mine()
+
+            frequentPattern = obj.getPatterns()
+
+            print("Total number of Frequent Patterns:", len(frequentPattern))
+
+            obj.save(oFile)
+
+            Df = obj.getPatternInDataFrame()
+
+            memUSS = obj.getMemoryUSS()
+
+            print("Total Memory in USS:", memUSS)
+
+            memRSS = obj.getMemoryRSS()
+
+            print("Total Memory in RSS", memRSS)
+
+            run = obj.getRuntime()
+
+            print("Total ExecutionTime in seconds:", run)
+
+
+    **Credits:**
+    ------------
+
+             The complete program was written by P.Likhitha  under the supervision of Professor Rage Uday Kiran.
 
     """
-        :Description: Apriori is one of the fundamental algorithm to discover frequent patterns in a transactional database. This program employs apriori property (or downward closure property) to  reduce the search space effectively. This algorithm employs breadth-first search technique to find the complete set of frequent patterns in a transactional database.
-
-        :Reference:  Agrawal, R., Imieli ́nski, T., Swami, A.: Mining association rules between sets of items in large databases.
-                In: SIGMOD. pp. 207–216 (1993), https://doi.org/10.1145/170035.170072
-
-        :param  iFile: str :
-                       Name of the Input file to mine complete set of frequent patterns
-        :param  oFile: str :
-                       Name of the output file to store complete set of frequent patterns
-        :param  minSup: int or float or str :
-                       The user can specify minSup either in count or proportion of database size. If the program detects the data type of minSup is integer, then it treats minSup is expressed in count. Otherwise, it will be treated as float.
-        :param  sep: str :
-                       This variable is used to distinguish items from one another in a transaction. The default seperator is tab space. However, the users can override their default separator.
-    """
-
 
     _minSup = float()
     _startTime = float()
     _endTime = float()
     _finalPatterns = {}
-    _iFile = ""
-    _oFile = ""
-    _sep = ""
+    _iFile = " "
+    _oFile = " "
+    _sep = " "
     _memoryUSS = float()
     _memoryRSS = float()
     _Database = []
 
     def _creatingItemSets(self) -> None:
-
+        """
+        Storing the complete transactions of the database/input file in a database variable
+        """
         self._Database = []
         if isinstance(self._iFile, _ab._pd.DataFrame):
             temp = []
@@ -101,7 +196,18 @@ class Apriori(_ab._frequentPatterns):
                     quit()
 
     def _convert(self, value: Union[int, float, str]) -> Union[int, float]:
+        """
+        To convert the user specified minSup value
 
+        :param value: user specified minSup value
+
+        :type value: int or float or str
+
+        :return: converted type
+
+        :rtype: int or float
+
+        """
         if type(value) is int:
             value = int(value)
         if type(value) is float:
@@ -114,12 +220,18 @@ class Apriori(_ab._frequentPatterns):
                 value = int(value)
         return value
 
+    @deprecated(
+        "It is recommended to use 'mine()' instead of 'startMine()' for mining process. Starting from January 2025, 'startMine()' will be completely terminated.")
     def startMine(self) -> None:
-
+        """
+        Frequent pattern mining process will start from here
+        """
         self.mine()
 
     def mine(self) -> None:
-
+        """
+        Frequent pattern mining process will start from here
+        """
         self._Database = []
         self._startTime = _ab._time.time()
 
@@ -150,11 +262,10 @@ class Apriori(_ab._frequentPatterns):
             else:
                 break
 
-
         while cands:
             newKeys = []
             for i in range(len(cands)):
-                for j in range(i+1, len(cands)):
+                for j in range(i + 1, len(cands)):
                     if cands[i][:-1] == cands[j][:-1]:
                         newCand = cands[i] + tuple([cands[j][-1]])
                         intersection = fileData[tuple([newCand[0]])]
@@ -167,7 +278,7 @@ class Apriori(_ab._frequentPatterns):
             del cands
             cands = newKeys
             del newKeys
-            
+
         process = _ab._psutil.Process(_ab._os.getpid())
         self._endTime = _ab._time.time()
         self._memoryUSS = float()
@@ -177,18 +288,54 @@ class Apriori(_ab._frequentPatterns):
         print("Frequent patterns were generated successfully using Apriori algorithm ")
 
     def getMemoryUSS(self) -> float:
+        """
+
+        Total amount of USS memory consumed by the mining process will be retrieved from this function
+
+        :return: returning USS memory consumed by the mining process
+
+        :rtype: float
+
+        """
 
         return self._memoryUSS
 
     def getMemoryRSS(self) -> float:
+        """
+
+        Total amount of RSS memory consumed by the mining process will be retrieved from this function
+
+        :return: returning RSS memory consumed by the mining process
+
+        :rtype: float
+
+        """
 
         return self._memoryRSS
 
     def getRuntime(self) -> float:
+        """
+
+        Calculating the total amount of runtime taken by the mining process
+
+        :return: returning total amount of runtime taken by the mining process
+
+        :rtype: float
+
+        """
 
         return self._endTime - self._startTime
 
     def getPatternsAsDataFrame(self) -> _ab._pd.DataFrame:
+        """
+
+        Storing final frequent patterns in a dataframe
+
+        :return: returning frequent patterns in a dataframe
+
+        :rtype: pd.DataFrame
+
+        """
 
         dataFrame = {}
         data = []
@@ -199,7 +346,17 @@ class Apriori(_ab._frequentPatterns):
         return dataFrame
 
     def save(self, outFile) -> None:
+        """
 
+        Complete set of frequent patterns will be loaded in to an output file
+
+        :param outFile: name of the output file
+
+        :type outFile: csvfile
+
+        :return: None
+
+        """
         self._oFile = outFile
         writer = open(self._oFile, 'w+')
         for x, y in self._finalPatterns.items():
@@ -207,10 +364,21 @@ class Apriori(_ab._frequentPatterns):
             writer.write("%s \n" % s1)
 
     def getPatterns(self) -> Dict[str, int]:
+        """
 
+        Function to send the set of frequent patterns after completion of the mining process
+
+        :return: returning frequent patterns
+
+        :rtype: dict
+
+        """
         return self._finalPatterns
 
     def printResults(self) -> None:
+        """
+        This function is used to print the result
+        """
         print("Total number of Frequent Patterns:", len(self.getPatterns()))
         print("Total Memory in USS:", self.getMemoryUSS())
         print("Total Memory in RSS", self.getMemoryRSS())
